e244cb38d60674cc45ebb7ba2e3ac153d567e311
dhasane <danihas@live.com>
2023-02-15 20:50:51 -0500

    I almost forgot the test for DriverController

---
+++ tests/controller/driver_controller_test.rb
@@ -0,0 +1,10 @@
+require 'minitest/autorun'
+require_relative '../db_helper'
+# require_relative '../../controllers/driver_controller'
+
+class DriverControllerTest < Minitest::Test
+  def setup
+    # @ = Driver.new
+  end
+
+end
